1fbe44b33c637b3d74e4b781ae116855d3b3740a
Chase Crowder <gcrowder93@gmail.com>
2021-11-30 17:56:42 -0800

    added footer

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import './Zodiac.css'
 
-// export default function Zodiac() {
-//   return (
-//     <>
-//       <div className="zodiac"></div>
-//     </>
-//   )
-// }
-
 export default function Zodiac(props) {
   return (
     <>
